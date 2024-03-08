c25265a6fd800e8f2fbb0524cec07504f863bbda
Documenter.jl <documenter@juliadocs.github.io>
2024-03-08 16:51:09 +0000

    delete history

---
+++ v1.0.7/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyCompatPkg","category":"page"},{"location":"#MyCompatPkg","page":"Home","title":"MyCompatPkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyCompatPkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyCompatPkg]","category":"page"}]
+}
